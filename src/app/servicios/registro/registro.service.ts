3090c7ce572ee04e2f71995657f85e92c835eaae
Demathal <gabriel.segura.castillo@cftmail.cl>
2024-11-21 21:50:02 -0300

    Mi primer Angular de servicios Backend

---
+++ src/app/servicios/registro/registro.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../../environments/environment.development';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistroService {
+  backend = environment.backend+"/registroForm";
+  constructor( private http: HttpClient) { }
+
+  obtenerRegistro (){
+    return this.http.get(`${this.backend}/obtener-registros`);
+  }
+}
